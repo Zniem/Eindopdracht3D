76e12cb44b78d02536681315c7f0a66ca0722e0f
siemh <siemhorst0@gmail.com>
2025-11-05 13:46:05 +0100

    last few things for GameObject

--- GameObject.cpp
+++ GameObject.cpp
@@ -2,32 +2,31 @@
 #include "ObjModel.h"
 #include "tigl.h"
 
+//Constructor
 GameObject::GameObject(ObjModel* model, glm::vec3 translate, float rotation, glm::vec3 scale)
-{
-	objectModel = model;
-	objectTranslate = translate;
-	objectRotation = rotation;
-	objectScale = scale;
+	: objectModel(model), objectTranslate(translate), objectRotation(rotation), objectScale(scale)
+{}
+
+//Method for drawing an object
+void GameObject::DrawObject(){
+	tigl::shader->setModelMatrix(GetModelMatrix());
+	objectModel->draw();
 }
 
-void GameObject::DrawObject() const {
+//Method for getting the current matrix
+glm::mat4 GameObject::GetModelMatrix() {
 	glm::mat4 modelMatrix = glm::mat4(1.0f);
 	modelMatrix = glm::translate(modelMatrix, objectTranslate);
 	modelMatrix = glm::rotate(modelMatrix, objectRotation, glm::vec3(0, 1, 0));
 	modelMatrix = glm::scale(modelMatrix, objectScale);
-	tigl::shader->setModelMatrix(modelMatrix);
-	objectModel->draw();
+	return modelMatrix;
 }
 
-void GameObject::SetTranslate(glm::vec3 translate) {
-	objectTranslate = translate;
-}
-void GameObject::SetRotation(float rotation) {
-	objectRotation = rotation;
-}
-void GameObject::SetScale(glm::vec3 scale) {
-	objectScale = scale;
-}
+//Setters
+void GameObject::SetTranslate(glm::vec3 translate) {objectTranslate = translate;}
+void GameObject::SetRotation(float rotation) {objectRotation = rotation;}
+void GameObject::SetScale(glm::vec3 scale) {objectScale = scale;}
 
+//Default constructor and deconstructor
 GameObject::~GameObject() {}
 GameObject::GameObject() {}
--- GameObject.h
+++ GameObject.h
@@ -4,20 +4,24 @@
 class GameObject
 {
 private:
+	//GameObject variables
 	ObjModel* objectModel;
 	glm::vec3 objectTranslate;
 	glm::vec3 objectScale;
 	float objectRotation;
-public:
-	GameObject();
-	GameObject(ObjModel* model, glm::vec3 translate,float rotation, glm::vec3 scale);
-	~GameObject();
 
-	void DrawObject() const;
+public:
+	GameObject(ObjModel* model, glm::vec3 translate,float rotation, glm::vec3 scale);
+
+	void DrawObject();
+
+	glm::mat4 GetModelMatrix();
 
 	void SetTranslate(glm::vec3 translate);
 	void SetRotation(float rotation);
 	void SetScale(glm::vec3 scale);
 
+	GameObject();
+	~GameObject();
 };
 
